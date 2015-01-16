f2804de7da53125055ccaf8d61fd50c0f369cd57
Jürn Brodersen <juern@uni-bremen.de>
2015-01-16 21:46:26 +0100

    Updated gitignore and path to cover place holder

--- lib/variables.py
+++ lib/variables.py
@@ -18,7 +18,7 @@ date_fields=['release_date', 'finish_date', 'start_date', 'add_date']
 
 def book_empty_default():
     book_empty = {name : '' for name in fieldnames}
-    book_empty['front'] =  'icons/circle-x.svg'
+    book_empty['front'] =  'static/icons/circle-x.svg'
     book_empty['_id'] = 'new_book'
     book_empty['type'] = 'book'
     book_empty['reading_stats'] = None
