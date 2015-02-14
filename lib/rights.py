1d97dbcf5ef20982a9ff9589e2e21b2b8f8fb5fa
Jürn Brodersen <juern@uni-bremen.de>
2015-02-14 01:32:39 +0100

    More bugs in lib/rights.py

--- lib/rights.py
+++ lib/rights.py
@@ -2,18 +2,14 @@ import cherrypy
 from lib.mongo import mongo_user, mongo_user_rights
 
 def check_rights():
-    view_user = cherrypy.request.path_info.split("/")[2]
-    user = mongo_user_rights(view_user)
-    if user == None:
+    temp = cherrypy.request.path_info.split("/")[2]
+    view_user = mongo_user_rights(temp)
+    if view_user == None:
         raise cherrypy.HTTPError(404, "Profile not found")
-    elif 'privacy' not in user or user['privacy'] == 'private':
-        try:
-            username = cherrypy.session['username']
-        except KeyError:
-            raise cherrypy.HTTPRedirect("/")
-        except AssertionError:
-            raise cherrypy.HTTPRedirect("/")
-        if username != view_user:
-            raise cherrypy.HTTPRedirect("/")
-    
+    elif 'privacy' not in view_user or view_user['privacy'] == 'private':
+        user = mongo_user(cherrypy.session.id)
+        if user != None and user['username'] == view_user['username']:
+          return
+        raise cherrypy.HTTPRedirect("/")
+
 cherrypy.tools.rights = cherrypy.Tool('before_handler', check_rights)
